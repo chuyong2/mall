bc79ec00f2092472602692f24226c1c4b9569293
pingdexiang <p2412658382@163.com>
2023-01-12 21:00:46 +0800

    Springboot+Mybatis搭建基本骨架

--- src/main/java/com/pdx/config/MybatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.pdx.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.pdx.mbg.mapper")
-public class MybatisConfig {
-}
