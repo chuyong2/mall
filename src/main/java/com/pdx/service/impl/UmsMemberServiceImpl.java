366d989a05db9433dadceb1158368114f0df6fdd
pingdexiang <p2412658382@163.com>
2023-01-16 20:42:52 +0800

    Redis整合实现手机验证码缓存

---
+++ src/main/java/com/pdx/service/impl/UmsMemberServiceImpl.java
@@ -0,0 +1,54 @@
+package com.pdx.service.impl;
+
+import com.alibaba.druid.util.StringUtils;
+import com.pdx.common.api.CommonResult;
+import com.pdx.service.RedisService;
+import com.pdx.service.UmsMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class UmsMemberServiceImpl implements UmsMemberService {
+    @Autowired
+    private RedisService redisService;
+    @Value("${redis.key.prefix.authCode}")
+    private String REDIS_KEY_PREFIX_AUTH_CODE;
+
+    //验证码超时时间
+    @Value("${redis.key.expire.authCode}")
+    private Long AUTH_CODE_EXPIRE_SECONDS;
+
+    @Override
+    public CommonResult generateAuthCode(String telephone) {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 6; i++) {
+            sb.append(random.nextInt(10));
+        }
+        //验证码绑定手机号并存储到redis
+        redisService.set(REDIS_KEY_PREFIX_AUTH_CODE + telephone, sb.toString());
+        //设计生存时间
+        redisService.expire(REDIS_KEY_PREFIX_AUTH_CODE + telephone,AUTH_CODE_EXPIRE_SECONDS);
+        return CommonResult.success(sb.toString(),"验证码获取成功");
+    }
+
+
+    //对输入的验证码进行校验
+    @Override
+    public CommonResult verifyAuthCode(String telephone, String authCode) {
+        if (StringUtils.isEmpty(authCode)) {
+            return CommonResult.failed("请输入验证码");
+        }
+        //获取实际验证码
+        String realAuthCode = redisService.get(REDIS_KEY_PREFIX_AUTH_CODE + telephone);
+        boolean result = authCode.equals(realAuthCode);
+        if (result) {
+            return CommonResult.success(null, "验证码校验成功");
+        } else {
+            return CommonResult.failed("验证码不正确");
+        }
+    }
+}
