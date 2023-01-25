46b01f248e93ebdf2f61bdeda2eae15d5fddde74
pingdexiang <p2412658382@163.com>
2023-01-25 20:33:06 +0800

    完善spring Security+Jwt实现认证和授权

--- src/main/java/com/pdx/MallApplication.java
+++ src/main/java/com/pdx/MallApplication.java
@@ -1,9 +1,11 @@
 package com.pdx;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.pdx.dao")
 public class MallApplication {
 
     public static void main(String[] args) {
